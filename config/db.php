ffd3695a46af169bbc78675d7532b4cb88edba7c
Beatriz <bestevesgoncalves@gmail.com>
2024-11-08 01:34:15 -0400

    Correcao no roteamento

---
+++ config/db.php
@@ -0,0 +1,12 @@
+<?php
+    $host = 'localhost';
+    $db = 'super8';
+    $user = 'postgres';
+    $pass = '123';
+
+    try {
+        $pdo = new PDO("pgsql:host=$host;dbname=$db", $user, $pass);
+        $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    } catch (PDOException $e) {
+        echo "Erro: " . $e->getMessage();
+    }
--- public/index.php
+++ public/index.php
@@ -1,24 +1,27 @@
 <?php
 require_once '../config/db.php';
 require_once '../controllers/Atleta.php';
+require_once '../controllers/Confronto.php';
+require_once '../Router.php';
 
 
-header("Content-type: ap[lication/json; charset=UTF-8");
+header("Content-type: aplication/json; charset=UTF-8");
 
 $router = new Router();
-$controller = new AtletaController($pdo);
+$atletaController = new AtletaController($pdo);
+$confrontoController = new ConfrontoController($pdo);
 
-$router->add('GET', '/atleta', [$controller, 'list']);
-$router->add('GET', '/atleta/{id}', [$controller, 'getById']);
-$router->add('POST', '/atleta', [$controller, 'create']);
-$router->add('DELETE', '/atleta/{id}', [$controller, 'delete']);
-$router->add('PUT', '/atleta/{id}', [$controller, 'update']);
+$router->add('GET', '/atleta', [$atletaController, 'list']);
+$router->add('GET', '/atleta/{id}', [$atletaController, 'getById']);
+$router->add('POST', '/atleta', [$atletaController, 'create']);
+$router->add('DELETE', '/atleta/{id}', [$atletaController, 'delete']);
+$router->add('PUT', '/atleta/{id}', [$atletaController, 'update']);
 
-$router->add('GET', '/confronto', [$controller, 'list']);
-$router->add('GET', '/confronto/{id}', [$controller, 'getById']);
-$router->add('POST', '/confronto', [$controller, 'create']);
-$router->add('DELETE', '/confronto/{id}', [$controller, 'delete']);
-$router->add('PUT', '/confronto/{id}', [$controller, 'update']);
+$router->add('GET', '/confronto', [$confrontoController, 'list']);
+$router->add('GET', '/confronto/{id}', [$confrontoController, 'getById']);
+$router->add('POST', '/confronto', [$confrontoController, 'create']);
+$router->add('DELETE', '/confronto/{id}', [$confrontoController, 'delete']);
+$router->add('PUT', '/confronto/{id}', [$confrontoController, 'update']);
 
 $requestedPath = parse_url($_SERVER["REQUEST_URI"], PHP_URL_PATH);
 $pathItems = explode("/", $requestedPath);
